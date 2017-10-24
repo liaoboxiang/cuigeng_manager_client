970bc606e3fcc7669ab0e4368430b69dde99e150
box <49935142@qq.com>
2017-10-24 16:45:56 +0800

    上传配置文件

--- main/main_upload_config.js
+++ main/main_upload_config.js
@@ -2,21 +2,55 @@
  * Created by box on 2017/10/23.
  * 主进程 上传配置
  */
-const path = require('path')
-const electron = require('electron')
+const electron = require('electron');
+const dialog = electron.dialog;
 const ipc = electron.ipcMain;
+var XLSX = require('xlsx');
+var request = require('request');
+var config = require('./config.json');
 
-ipc.on('upload_config', function (event, filePath) {
-    // dosomething
-    read_config_file(filePath);
-    event.sender.send('upload_finish');
-});
-
-ipc.on('select_config', function(event, filePath){
-    console.log("aaa", filePath);
+ipc.on('upload_config', function (event) {
+    dialog.showOpenDialog({
+        properties: [
+            'openFile',
+        ]
+    },function(res){
+        var filePath = res[0];
+        read_config_file(filePath, function(err, data){
+            if(err){
+                event.sender.send('upload_finish', err);
+            }else{
+                event.sender.send('upload_finish');
+            }
+        });
+    });
 });
 
 // 读取配置文件
-function read_config_file(filePath){
-    
+function read_config_file(filePath, cb){
+    var books = [];
+    var workbook = XLSX.readFile(filePath);
+    for(var sheet in workbook.Sheets){
+        if (workbook.Sheets[sheet]['!ref']) {
+            books.push({
+                name:sheet,
+                datas:XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
+            });
+        }
+    };
+    cb(null, books);
+};
+
+// 上传服务器
+function upload_to_server(data, cb){
+    var ip = config.server_ip;
+    var port = config.port;
+    var opt = {
+        url:ip + ":" + port + "/upload_config",
+        method:"GET",
+        data:data
+    }
+    request(opt, function(error, response, body){
+
+    })
 }
--- test.js
+++ test.js
@@ -5,10 +5,50 @@
 var fs = require('fs');
 var path = require('path');
 var parseXlsx = require('excel');
+var XLSX = require('xlsx');
+var request = require('request');
+var config = require('./config.json');
 
 var filePath = 'C:/Users/sscf/Desktop/电影.xlsx';
+//
+//parseXlsx(filePath, function(err, lines) {
+//    if(err) throw err;
+//    console.log(lines);
+//});
 
-parseXlsx(filePath, function(err, lines) {
-    if(err) throw err;
-    console.log(lines[2]);
-});
+function read_config_file(filePath){
+    var books = [];
+    var workbook = XLSX.readFile(filePath);
+    for(var sheet in workbook.Sheets){
+        if (workbook.Sheets[sheet]['!ref']) {
+            books.push({
+                name:sheet,
+                datas:XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
+            });
+        }
+    };
+   return books;
+};
+
+function upload_to_server(data){
+    var ip = "127.0.0.1";
+    var port = config.port;
+    var opt = {
+        url:"http://" + ip + ":" + port + "/upload_config",
+        method:"GET",
+        qs:{data:data}
+    };
+    console.log(opt);
+    request(opt, function(error, response, body){
+        if(error){
+            console.error("upload error", error);
+        }else{
+            console.log(body);
+        }
+    })
+}
+
+var data = read_config_file(filePath);
+upload_to_server(data);
+
+//console.log(XLSX.utils.sheet_to_json(workbook.Sheets[0]));
